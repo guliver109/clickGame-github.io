0c672c60ca36bc2b0132f082c208f387a300cc4f
Marko Gusic <markogusic@aol.com>
2018-10-23 15:23:19 -0700

    final code

---
+++ my-app/build/precache-manifest.b99c3ddc45e487fbc76e94284aede295.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a067fec4fb876b48487f",
+    "url": "/clickGame-github.io/static/js/runtime~main.a067fec4.js"
+  },
+  {
+    "revision": "bf58d80b4c9cbe341a5a",
+    "url": "/clickGame-github.io/static/js/main.bf58d80b.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clickGame-github.io/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "bf58d80b4c9cbe341a5a",
+    "url": "/clickGame-github.io/static/css/main.1435be62.chunk.css"
+  },
+  {
+    "revision": "da8c1aa65e3143b2f15c0a264c7df656",
+    "url": "/clickGame-github.io/index.html"
+  }
+];
