91fbbfa5ab18495f039e5c6aa625fd8544ac081a
Marko Gusic <markogusic@aol.com>
2018-10-23 15:24:02 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/clickGame/precache-manifest.0179f620aac21920ae04050c23d535ad.js"
+  "/clickGame-github.io/precache-manifest.b99c3ddc45e487fbc76e94284aede295.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/clickGame/index.html", {
+workbox.routing.registerNavigationRoute("/clickGame-github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
